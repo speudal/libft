f173aa0e7e366a026762975b0c10f0248c376f77
tduval <tduval@student.42.fr>
2019-03-03 02:29:47 +0100

    safe

--- srcs/ft_memcpy.c
+++ srcs/ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tduval <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 03:54:55 by tduval            #+#    #+#             */
-/*   Updated: 2018/11/09 16:15:33 by tduval           ###   ########.fr       */
+/*   Updated: 2019/03/03 02:29:36 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	i = 0;
 	dst1 = (unsigned char *)dst;
 	src1 = (unsigned char *)src;
-	while (n--)
+	while (dst1 && src1 && dst1[i] && src1[i] && n--)
 	{
 		dst1[i] = src1[i];
 		i++;
